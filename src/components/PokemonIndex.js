a8cd9418307aef027828589f1c1784d8fa2e8faf
patti-c <patricia.carter@flatironschool.com>
2018-11-04 15:10:56 -0500

    Done.

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -3,16 +3,71 @@ import PokemonCollection from './PokemonCollection'
 import PokemonForm from './PokemonForm'
 import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
+import Adapter from './Adapter'
+const adapter = new Adapter();
 
 class PokemonPage extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      pokemons: [],
+      searchTerm: ''
+    }
+  }
+
+  componentDidMount() {
+    adapter.fetchAllPokemon().then(this.addPokemonsToState)
+  }
+
+  addPokemonsToState = (pokemonData) => {
+    this.setState({
+      pokemons: this.pokemonParser(pokemonData)
+    })
+    return this.state.pokemons
+  }
+
+  pokemonParser(data) {
+    const oldPokemon = data.filter(mon => mon.userAdded === undefined)
+    const newPokemon = data.filter(mon => mon.userAdded === true)
+    const parsedNewPokemon = newPokemon.map(function(mon) {
+      return ({
+        "name": mon.name,
+        "stats": [{},{},{},{},{},{"name": "hp", "value": mon.hp}],
+        "sprites": {
+          "front": mon.frontUrl,
+          "back": mon.backUrl
+        },
+        "id": mon.id}
+      )
+    })
+
+    return oldPokemon.concat(parsedNewPokemon)
+
+  }
+
+  handleSearch = (e, {value}) => {
+    this.setState({searchTerm: value})
+  }
+
+  getSpecificPokemons(substring) {
+    if(substring === '') {
+      return []
+    } else if(substring === 'all'){
+      return this.state.pokemons
+    } else {
+      return this.state.pokemons.filter(mon => mon.name.includes(substring))
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={_.debounce(this.handleSearch, 500)} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={this.getSpecificPokemons(this.state.searchTerm)} />
         <br />
         <PokemonForm />
       </div>
