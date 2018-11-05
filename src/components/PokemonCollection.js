0554e90710f888e33093f29ae3d20fd73d08dc4a
Srikant Kumar Kalaputapu <skalaputapu@gmail.com>
2018-11-05 17:57:11 -0500

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,10 +3,15 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+
+    //conditional rendering
+
   render() {
+
     return (
       <Card.Group itemsPerRow={6}>
         <h1>Hello From Pokemon Collection</h1>
+        {this.props.pokemons.map(poke => {return <PokemonCard key={poke.id} flipImage={this.props.flipImage} pokemon={poke} />})}
       </Card.Group>
     )
   }
